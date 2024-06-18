1ea0359fbc1f4056cd1b1e546097bf4b20b25732
Nathan Caldas <133052483+Najatnos@users.noreply.github.com>
2024-06-18 19:02:41 -0300

    Update SaborExpress.py

--- SaborExpress.py
+++ SaborExpress.py
@@ -1,42 +1,68 @@
 from unidecode import unidecode
+import time
+TipoBusca = None
 Restaurantes = [{'Nome':'Subauru', 'Categoria':'Fast food', 'Cargo-chefe':'Bauru', 'Status':True},
                 {'Nome':'MuyGuápo', 'Categoria':'Mexicana', 'Cargo-chefe':'Taco', 'Status':True},
-                {'Nome':'Ice, Ice Baby!', 'Categoria':'Sorveteria', 'Cargo-chefe':'Açai', 'Status':False}]
+                {'Nome':'IceIceBaby', 'Categoria':'Sorveteria', 'Cargo-chefe':'Açai', 'Status':False}]
 def NeutralizadorAcento(texto):
     return unidecode(texto)
 def Pula():
-    r = input('Volte ao menu digitando "r": ')
+    r = input('\nVolte ao menu digitando "r": ')
     if r == 'r': print("\033[H\033[J", end=""), main()
     else: Encerrar()
 def LimparTerminal():
     print("\033[H\033[J", end="")
 def Encerrar():
+    LimparTerminal()
     TituloDecisao('\033[1mFINALIZANDO O SISTEMA.\033[0m \n')
 def Error():
-    print("\033[H\033[J", end="")
-    r = input('Volte ao menu digitando "r": ')
+    LimparTerminal()
+    r = input('\nVolte ao menu digitando "r": ')
     if r == 'r': main()
     else: Encerrar()
 def BuscaNome(NomeRestaurante, CatalogoRestaurante):
     NomeRestaurante = NeutralizadorAcento(NomeRestaurante.lower())  
-    return next((Restaurante['Nome'] for Restaurante in CatalogoRestaurante if NeutralizadorAcento(Restaurante['Nome'].lower()) == NomeRestaurante), None)
+    return next((Restaurante for Restaurante in CatalogoRestaurante if NeutralizadorAcento(Restaurante['Nome'].lower()) == NomeRestaurante), None)
+    return Buscado
 def TituloDecisao(titulo):
-    print("\033[H\033[J", end="")
     print(titulo)
     print()
 def Listar():
+    LimparTerminal()
     TituloDecisao('\033[1mLISTAMENTO DE RESTAURANTES.\033[0m \n')
+    print(f'{'NOME'.ljust(22)} | {'CATEGORIA'.ljust(20)} | {'CARGO-CHEFE'.ljust(20)} | STATUS')
     for Restaurante in Restaurantes:
         NomeRestaurante = Restaurante['Nome']
         CategoriaRestaurante = Restaurante['Categoria']
         CargoChefeRestaurante = Restaurante['Cargo-chefe']
-        StatusRestaurante = Restaurante['Status']
-        print(f'• {NomeRestaurante} - {CategoriaRestaurante} - {CargoChefeRestaurante} - {StatusRestaurante}')
-        print
+        StatusRestaurante = 'Ativo' if Restaurante['Status'] else 'Inativo'
+        print(f'• {NomeRestaurante.ljust(20)} - {CategoriaRestaurante.ljust(20)} - {CargoChefeRestaurante.ljust(20)} - {StatusRestaurante}')
     Pula()
-def AtivarDesativar():
-    print('Ativar restaurante')
+def ProcuraNome(TipoBusca):
+    while True:
+        LimparTerminal()
+        TituloDecisao('\033[1mPROCURANDO NOME.\033[0m \n')
+        Busca = input("Digite o nome do restaurante: ")
+        Buscado = BuscaNome(Busca, Restaurantes)
+        if Buscado:
+            print("O restaurante '{}' foi encontrado! (Espere cinco segundos)".format(Buscado['Nome']))
+            if TipoBusca == 'Ativa/Desativa.': 
+                time.sleep(5)
+                LimparTerminal()
+                AtivarDesativar(Buscado)
+            break        
+        else:
+            print(f"O restaurante '{Busca}' não foi encontrado. Tente novamente nos próximos doze segundos.")
+            time.sleep(12)
+            LimparTerminal()
+def AtivarDesativar(Restaurante):
+    TituloDecisao('\033[1mATIVAR/DESATIVAR.\033[0m \n')
+    Restaurante['Status'] = not Restaurante['Status']
+    NovoStatus = "Ativo" if Restaurante['Status'] else "Inativo"
+    print("Agora, o restaurante está: ", NovoStatus)
+    Pula()
 def Cadastrar():
+    LimparTerminal()
     TituloDecisao('\033[1mCADASTRO DE RESTAURANTES.\033[0m \n')
     RestauranteNome = input('Nome: ')
     while len(RestauranteNome) == 0: RestauranteNome = input('Não digitaste. Tente novamente. \nNome: ')
@@ -57,23 +83,22 @@ def MostreNomeDoApp():
 def MostreOpcoesRestaurante():
     print(f'\n 1. Cadastrar restaurante\n 2. Listar restaurante\n 3. Ativar restaurante\n 4. Sair\n')
 def Decida():
+    global TipoBusca
     try:
         OpcaoEscolhida = int(input('Qual sua escolha? '))
-        if OpcaoEscolhida in [1, 2, 3]:
-            print(f'\nOpção {OpcaoEscolhida} á caminho!'.format(OpcaoEscolhida = OpcaoEscolhida))
-        match OpcaoEscolhida:
-            case 1:
+        if OpcaoEscolhida == 1:
                 Cadastrar()
-            case 2:
+        elif OpcaoEscolhida == 2:
                 Listar()
-            case 3:
-                AtivarDesativar()
-            case 4:
+        elif OpcaoEscolhida == 3:
+                TipoBusca = 'Ativa/Desativa.'
+                ProcuraNome(TipoBusca)
+        elif OpcaoEscolhida == 4:
                 LimparTerminal()
                 Encerrar()
-            case _:
+        else :
                 Error()
-    except:
+    except ValueError:
         Error()
     #Inteiro = int(OpcaoEscolhida)
 def main():
