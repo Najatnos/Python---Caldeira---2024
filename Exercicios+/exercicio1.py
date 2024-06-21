49134909655e40ff0ac36b9644c85ffc3c75bca6
Nathan Caldas <133052483+Najatnos@users.noreply.github.com>
2024-06-21 14:03:08 -0300

    Add files via upload

---
+++ Exercicios+/exercicio1.py
@@ -0,0 +1,19 @@
+
+print('Python na Escola de Programação da Alura')
+#Exercicio numero 1
+
+nome = input('\nQual seu nome? ')
+idade = input('\nQual sua idade? ')
+print(f'\nMeu nome é {nome} e tenho {idade} anos')
+#Exercicio numero 2
+
+pi_arredondado = round(3.14159, 2)
+print(f'\nO valor arredondado de pi é: {pi_arredondado}\n')
+#Exercicio numero 4
+
+print('\033[31;1mA\033[0m')
+print('L')
+print('U')
+print('R')
+print('\033[31;1mA\033[0m')
+#Exercicio numero 3
